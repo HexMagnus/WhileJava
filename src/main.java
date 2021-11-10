810d421856b71d7cac011751bed8e569f3c8a708
HexMagnus <luke.dev.fullstack@outlook.com>
2021-11-10 04:52:36 -0300

    Update
    Update

---
+++ src/main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class main {
+    
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        //Declaração Scanner para sc
+
+        int x = sc.nextInt();
+
+        //Variavel de número inteiro valor x, imprme e passa para o proximo int.
+
+        while (x !=0){
+            x = sc.nextInt();
+        }
+        sc.close();
+
+        // While ou Enquanto. X é diferente de 0 ele continua imprimindo na tela, quando for diferente, para a execução.
+    }
+        
+    }
+
